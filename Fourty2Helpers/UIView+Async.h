5f0dc226b8535052a4f3d750dba21d79d6ba3b35
Omid Hashemi <omid@42dp.com>
2012-11-18 13:22:06 +0100

    async NSData Download

---
+++ Fourty2Helpers/UIView+Async.h
@@ -0,0 +1,12 @@
+//
+// Created by omid on 11/16/12.
+//
+// Copyright (c) 2012 42dp. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@interface UIView (Async)
+
+@end
---
+++ Fourty2Helpers/UIView+Async.m
@@ -0,0 +1,12 @@
+//
+// Created by omid on 11/16/12.
+//
+// Copyright (c) 2012 42dp. All rights reserved.
+//
+
+
+#import "UIView+Async.h"
+
+
+@implementation UIView (Async)
+@end
